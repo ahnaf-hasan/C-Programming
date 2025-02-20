c24a6251980891ae9b76255a31f893bf1c81afec
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-02-20 13:17:20 +0600

    add new file

---
+++ integer.c
@@ -0,0 +1,16 @@
+//Let's Start
+
+#include <stdio.h>
+int main() {
+    int number;
+
+    printf("Enter an integer: ");
+
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("The number is: %d", number);
+
+    return 0;
+}
