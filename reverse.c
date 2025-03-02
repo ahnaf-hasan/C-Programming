b2ab9a3783f257e789c949e2b9fa4a19dfcfa68e
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-03-02 22:53:12 +0600

    add new file

---
+++ reverse.c
@@ -0,0 +1,22 @@
+//Let's Start
+#include <stdio.h>
+#include <string.h>
+
+// Function to reverse a string
+void reverseString(char* str) {
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        // Swap characters
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
+
+int main() {
+    char str[] = "MADAM!";
+    printf("Original string: %s\n", str);
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
