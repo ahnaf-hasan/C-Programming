33c6f825aea09e745161ef2712f4f9e0a83d6979
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-03-04 04:39:13 +0600

    add new file

---
+++ loop.c
@@ -0,0 +1,14 @@
+//Let's Start
+#include <stdio.h>
+
+int main() {
+    // Declare a counter variable
+    int i;
+    
+    // For loop to print numbers from 1 to 10
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
