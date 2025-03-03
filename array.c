5083b8c1cb0518599f1d2e91b59c164b5fdc8275
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-03-03 14:17:51 +0600

    new add file

---
+++ array.c
@@ -0,0 +1,17 @@
+//Let's Start
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    // Loop through the array and calculate the sum
+    for(int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    // Print the sum
+    printf("The sum of the array elements is: %d\n", sum);
+
+    return 0;
+}
