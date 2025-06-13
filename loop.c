7105e530d274b3fa342bb64a7d1521d9a934fe76
ahnaf-hasan <ahnaf.cse8741@gmail.com>
2025-06-13 13:49:10 +0600

    update

--- loop.c
+++ loop.c
@@ -5,8 +5,8 @@ int main() {
     // Declare a counter variable
     int i;
     
-    // For loop to print numbers from 1 to 10
-    for (i = 1; i <= 10; i++) {
+    // For loop to print numbers from 1 to 99
+    for (i = 1; i <= 99; i++) {
         printf("%d\n", i);
     }
     
